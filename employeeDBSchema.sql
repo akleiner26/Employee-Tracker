f15ae09e7e7d04635633e75b4b65fad86064c2c5
orangeblack13 <AndrewJKleiner@gmail.com>
2020-07-10 17:24:34 -0400

    schema and seeds working, view all employees function being worked on

--- employeeDBSchema.sql
+++ employeeDBSchema.sql
@@ -15,18 +15,21 @@ CREATE TABLE role(
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(20,2),
     department_id INT(10), 
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE employee(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL, 
     role_id INT(10),
-    manager_id INT(10),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
+    manager_id INT(10) DEFAULT NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    INDEX man_ind (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
     PRIMARY KEY (id)
 )
+
+
 
--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -8,7 +8,6 @@ INSERT INTO role (title, salary, department_id)
 VALUES ("Sales Lead", 100000, 1), ("Salesperson", 80000, 1), ("Lead Engineer", 150000, 2), ("Software Engineer", 120000, 2), ("Accountant", 125000, 3), ("Legal Team Lead", 250000, 4), ("Lawyer", 190000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jane", "Doe", 1, 0), ("Michael", "Chan", 2, 1), ("Ryan", "Rodriguez", 3, 0), ("Michael", "Tupik", 4, 3), ("Miguel", "Brown", 5, 0), ("Jared", "Lourd", 6, 0), ("Jodie", "Allen", 7, 6);
+VALUES ("Jane", "Doe", 1, null), ("Michael", "Chan", 2, 1), ("Ryan", "Rodriguez", 3, null), ("Michael", "Tupik", 4, 3), ("Miguel", "Brown", 5, null), ("Jared", "Lourd", 6, null), ("Jodie", "Allen", 7, 6);
 
 
-Error Code: 1452. Cannot add or update a child row: a foreign key constraint fails (`employeesdb`.`employee`, CONSTRAINT `fk_employee` FOREIGN KEY (`manager_id`) REFERENCES `employee` (`id`) ON DELETE SET NULL)
